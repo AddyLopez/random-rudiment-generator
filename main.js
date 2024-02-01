a9a1217238b7de377431997dfd55faf57817ab17
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2024-02-01 00:13:07 -0700

    Test presentation of all images and fix filepaths

--- main.js
+++ main.js
@@ -8,18 +8,18 @@ const container = document.getElementById("img-container");
 const fragment = document.createDocumentFragment(); // See https://developer.mozilla.org/en-US/docs/Web/API/Document/createDocumentFragment#usage_notes
 
 rudiments.forEach((rudiment) => {
-  if (rudiment["id"] === "01" || rudiment["id"] === "02") {
-    const figure = document.createElement("figure");
-    const img = document.createElement("img");
-    const figcaption = document.createElement("figcaption");
-    img.src = rudiment["src"];
-    figcaption.textContent = `${rudiment["id"]}. ${rudiment["name"]}`;
-    figure.appendChild(img);
-    figure.appendChild(figcaption);
-    fragment.appendChild(figure);
-  } else {
+  /*if (rudiment["id"] === "01" || rudiment["id"] === "02") {*/
+  const figure = document.createElement("figure");
+  const img = document.createElement("img");
+  const figcaption = document.createElement("figcaption");
+  img.src = rudiment["src"];
+  figcaption.textContent = `${rudiment["id"]}. ${rudiment["name"]}`;
+  figure.appendChild(img);
+  figure.appendChild(figcaption);
+  fragment.appendChild(figure);
+  /*} else {
     return;
-  }
+  }*/
 });
 //console.log(fragment);
 container.appendChild(fragment);
