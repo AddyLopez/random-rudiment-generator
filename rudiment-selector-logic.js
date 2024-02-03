757ca8ca9ad64381dbc83d0d8ddf4bddddc13aa5
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2024-02-03 00:20:32 -0700

    Eliminate logging and add clarifying comments

--- rudiment-selector-logic.js
+++ rudiment-selector-logic.js
@@ -17,17 +17,18 @@ export const selectRandomRudiments = (categories, numberOfRudiments) => {
   /* To implement a recursive solution to ensure that only unique indices are included in a single category when more than one rudiment is selected results in a stack overflow. */
   const runSwitchStatement = () => {
     let count = numberOfRudiments;
+
+    // Edge case: When only the "Diddle Rudiments" category is selected, the count should be limited to 4 since there are no more than 4 diddle rudiments in the data set.
     if (categories.length === 1 && categories.includes("category2")) {
       count = 4;
     }
+    /* category1 (roll rudiments): indices 0 to 14,
+       category2 (diddle rudiments): indices 15 to 18,
+       category3 (flam rudiments): indices 19 to 29,
+       category4 (drag rudiments): indices 30 to 39,
+       max adjusted by +1 to account for the flooring function in getRandomIndex helper function */
     while (count) {
       categories.forEach((category) => {
-        /* category1 (roll rudiments): indices 0 to 14,
-             category2 (diddle rudiments): indices 15 to 18,
-             category3 (flam rudiments): indices 19 to 29,
-             category4 (drag rudiments): indices 30 to 39 
-             max adjusted by +1 to account for the flooring function in getRandomIndex helper function */
-
         switch (category) {
           case "category1":
             const rollIndex = getRandomIndex(0, 15);
@@ -52,16 +53,15 @@ export const selectRandomRudiments = (categories, numberOfRudiments) => {
       count--;
     }
   };
+
   runSwitchStatement();
-  console.log(indicesSelected);
 
   if (indicesSelected.length === numberOfRudiments) {
     for (let i = 0; i < 6; i++) {
       runSwitchStatement();
       // Generate more indices so that any duplicates in the array (especially probable as numberOfRudiments approaches 10) can be removed below. At that extreme, without a set larger than 10, the statistical likelihood of duplicates practically ensures a stack overflow in the code below, were it to allow a set of 10.
-      // Generate even more indices so that when category2 is selected the odds of randomly generating all four options are much higher to avoid a stack overflow.
+      // Generate even more indices so that when category2 is selected the odds of randomly generating all four options are statistically much higher to avoid a stack overflow.
     }
-    console.log(indicesSelected);
   }
 
   /* When multiple categories are checked, the number of randomized indices collected in indicesSelected will exceed the number of rudiments selected in the form submitted. This larger randomized set ensures that the final set of pared down selections contains no bias toward any given category of rudiments. */
@@ -84,7 +84,7 @@ export const selectRandomRudiments = (categories, numberOfRudiments) => {
             finalIndicesSelected.push(indicesSelected[indexSelected]);
             return;
           } else {
-            return selectIndexFromSet();
+            return selectIndexFromSet(); // Recursive function call.
           }
         };
         selectIndexFromSet();
@@ -94,7 +94,6 @@ export const selectRandomRudiments = (categories, numberOfRudiments) => {
     }
     indicesSelected = finalIndicesSelected; // This reassignment ensures that the proper number of rudiments selected is being respected.
   }
-  console.log(indicesSelected);
 
   return indicesSelected;
 };
