aa3e921340a1bb147e2ab822e5c23532907c2c15
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2024-02-03 00:04:43 -0700

    Eliminate logging and add clarifying comments

--- main.js
+++ main.js
@@ -1,40 +1,33 @@
 import { rudiments } from "./forty-essential-rudiments.js";
 import { selectRandomRudiments } from "./rudiment-selector-logic.js";
 
-const logData = (data) => console.log(data);
-logData(rudiments);
-
 const container = document.getElementById("img-container");
-const fragment = document.createDocumentFragment(); // See https://developer.mozilla.org/en-US/docs/Web/API/Document/createDocumentFragment#usage_notes
-
 const form = document.getElementById("form");
-console.log(form);
+const fragment = document.createDocumentFragment(); // See https://developer.mozilla.org/en-US/docs/Web/API/Document/createDocumentFragment#usage_notes
 
 const onSubmit = (event) => {
   event.preventDefault();
+
   // Clear the images when form is submitted again without a page refresh. That way, the newly generated rudiments won't appear after the previously generated list.
   while (container.firstChild) {
     container.removeChild(container.firstChild);
   }
 
-  const formData = new FormData(form);
+  const formData = new FormData(form); // Extract the submitted form's data.
+  const data = Object.fromEntries(formData); // Convert form's data to an object (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/fromEntries)
+  const numberOfRudiments = Number(data["number-of-rudiments"]); // Convert string to number to enable numerical operations and prevent type-errors.
 
-  const data = Object.fromEntries(formData); // convert form's data to object (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/fromEntries)
-  console.log(data);
+  const categories = Object.keys(data); // Step one to isolate in an array the categories checked by the user.
+  categories.shift(); // Step two: Easy way to eliminate "number-of-rudiments" from the array of keys in order to isolate just the keys pertaining to the categories selected in the checklist.
 
-  const numberOfRudiments = Number(data["number-of-rudiments"]);
-  console.log(typeof numberOfRudiments);
-
-  const categories = Object.keys(data);
-  categories.shift(); // Easiest way to eliminate "number-of-rudiments" from the array of keys in order to isolate just the keys pertaining to the categories selected in the checklist.
-  console.log(categories);
-
+  // Prevent program from running needlessly if no category is checked. Effectively sidesteps hairy implementation of a "required" attribute on checkboxes.
   if (categories.length > 0) {
     const finalSelections = selectRandomRudiments(
       categories,
       numberOfRudiments
     );
-    console.log(finalSelections);
+
+    // DOM manipulation if the randomized generation is successful.
     if (finalSelections) {
       finalSelections.forEach((finalSelection) => {
         const figure = document.createElement("figure");
